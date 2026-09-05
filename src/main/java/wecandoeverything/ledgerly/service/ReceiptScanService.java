90fd5fc9c1a62ebc076a3e39022f7bab67c69f38
yel-m <coin6442@gmail.com>
2026-09-05 16:04:13 +0900

    fix : update to cut floating point

--- src/main/java/wecandoeverything/ledgerly/service/ReceiptScanService.java
+++ src/main/java/wecandoeverything/ledgerly/service/ReceiptScanService.java
@@ -92,7 +92,7 @@ public class ReceiptScanService {
         return result.toBuilder()
                 .possibleDuplicate(true)
                 .duplicateNote("비슷한 요청" + result.getMerchant() +
-                        " (₩`" + result.getAmount() + ") 이 이미 있습니다.")
+                        " (₩`" + result.getAmount().intValue() + ") 이 이미 있습니다.")
                 .build();
     }
 
